d19391598a62be7c26914c44fdb710db766c3f1c
dwikysahut <dwikysahut@gmail.com>
2020-06-29 14:46:30 +0700

    fix code

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,13 +1,6 @@
 import * as Firebase from 'firebase';
 const firebaseConfig = {
-  apiKey: 'AIzaSyBWtQzMAET67Fmef0DTM33f44bDO0z102c',
-  authDomain: 'hell-o-chat-app.firebaseapp.com',
-  databaseURL: 'https://hell-o-chat-app.firebaseio.com',
-  projectId: 'hell-o-chat-app',
-  storageBucket: 'hell-o-chat-app.appspot.com',
-  messagingSenderId: '965675952937',
-  appId: '1:965675952937:web:9a5efc810908580d602538',
-  measurementId: 'G-5XTBSS79QK',
+//copy your code here
 };
 
 const fireApp = Firebase.initializeApp(firebaseConfig);
